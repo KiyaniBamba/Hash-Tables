61b91749bdad6bec4886f5bfc21ccdab94d2c50a
kiyani <bamba.kiyani@gmail.com>
2020-03-09 13:56:34 -0400

    WEBEU3 - Benjamin Grabow - Kiyani_Bamba - Week 5 day 1

--- src/hashtable.py
+++ src/hashtable.py
@@ -15,7 +15,7 @@ class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
+ko
 
     def _hash(self, key):
         '''
